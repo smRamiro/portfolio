c319f93e67075318f89f1a95eeca092ed915b534
Ramiro Sandoval <ramiro_sando@outlook.com>
2020-12-23 16:40:12 -0300

    Menu button done

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Select DOM items
 const menu = document.querySelector('.menu');
-const menuNav = document.querySelector('.btn-nav');
-const menuBtn = document.querySelector('.btn-menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBtn = document.querySelector('.menu-btn');
 const menuBranding = document.querySelector('.menu-branding');
 
 const navItems = document.querySelectorAll('.nav-item')
@@ -14,19 +14,19 @@ menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu(){
     if(!showMenu){
-        menuBtn.classList.add('close')
         menu.classList.add('show')
         menuNav.classList.add('show')
+        menuBtn.classList.add('close')
         menuBranding.classList.add('show')
-
-        for (let i = 0; i < navItems.length; i++) {
-            navItems[i].classList.add('show');
-        }
+        navItems.forEach(item => item.classList.add('show'))
 
         showMenu = true;
     }else{
-        menuBtn.classList.add('close')
-        menuBtn.classList.add('close')
+        menu.classList.remove('show')
+        menuNav.classList.remove('show')
+        menuBtn.classList.remove('close')
+        menuBranding.classList.remove('show')
+        navItems.forEach(item => item.classList.remove('show'))
 
         showMenu = false;
     }
